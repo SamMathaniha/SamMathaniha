172f04a756d88a4ca282fe9a6db5a4258b94eff2
Sam mathaniha <119151882+SamMathaniha@users.noreply.github.com>
2024-03-26 10:25:34 +0530

    Delete assets directory

--- assets/js/AllProjectSearch.js
+++
@@ -1,9 +0,0 @@
-
-  function search() {
-    var selectedValue = document.getElementById("projectSelect").value;
-    if(selectedValue !== "") {
-      window.location.href = "../MyEachProjects/"+ selectedValue + ".html";
-    } else {
-      alert("Please select a project.");
-    }
-  }
--- assets/js/CurrentYear.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the current year
-    var currentYear = new Date().getFullYear();
-  
-    // Display the current year inside the <p> tag
-    document.getElementById("currentYear").innerHTML = currentYear;
-  });
-  
